90300bb66bf797fa19c0087f993a3e574d76b7f3
Vishal Pant <>
2018-09-05 09:51:28 +0530

    money change in java

--- week3_greedy_algorithms/1_money_change/Change.java
+++ week3_greedy_algorithms/1_money_change/Change.java
@@ -3,7 +3,23 @@ import java.util.Scanner;
 public class Change {
     private static int getChange(int m) {
         //write your code here
-        return m;
+		int coins = 0;
+		if(m>=10)
+		{
+			coins+=m/10;
+			m%=10;
+		}
+		if(m>=5)
+		{
+			coins+=m/5;
+			m%=5;
+		}
+		if(m>=1)
+		{
+			coins+=m;
+			
+		}
+        return coins;
     }
 
     public static void main(String[] args) {
